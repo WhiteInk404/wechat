ed3086856d30cb9a264dd332450dc2ef60354951
jinchun <imjinchun@gmail.com>
2017-08-10 20:00:05 +0800

    添加微信服务器验证路由

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ Route::group(['middleware' => ['wechat.oauth:snsapi_userinfo']], function () {
     Route::get('activity/{activity_id}/team/{team_id}', 'HomeController@activityTeam')->name('activity_team');
 });
 
+Route::any('wechat/server', 'ServerController@server');
+
 Auth::routes();
