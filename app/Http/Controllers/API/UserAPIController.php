9ece01ea1c9cb7e8bd6285b080f75b66e49ed2a6
jinchun <imjinchun@gmail.com>
2017-08-13 02:42:29 +0800

    用户如果还没有背单词信息，预创建一份

--- app/Http/Controllers/API/UserAPIController.php
+++ app/Http/Controllers/API/UserAPIController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Entities\Wordbook;
+use App\Entities\WordbookState;
 use App\Http\Controllers\AppBaseController;
 use Auth;
 
@@ -9,8 +11,19 @@ class UserAPIController extends AppBaseController
 {
     public function me()
     {
-        $userinfo = Auth::user()->load(['wordbookState', 'reminder'])->append(['sign_count']);
+        $user = Auth::user()->append(['sign_count']);
 
-        return $this->sendResponse($userinfo->toArray());
+        // 如果还没有背单词信息，预创建一份
+        if (!$user->wordbookState) {
+            $wordbook       = Wordbook::orderByRaw('id asc,sort asc')->first();
+            $wordbook_state = new WordbookState([
+                'wordbook_id'    => $wordbook->id,
+                'word_total'     => $wordbook->contents()->count(),
+                'remember_total' => 0,
+            ]);
+            $user->wordbookState()->save($wordbook_state);
+        }
+
+        return $this->sendResponse($user->load(['wordbookState', 'reminder'])->toArray());
     }
 }
