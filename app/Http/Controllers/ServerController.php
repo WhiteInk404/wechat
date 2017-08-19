11283a2b90798da427dc1be68e4587074f5b6357
jinchun <imjinchun@gmail.com>
2017-08-19 23:59:04 +0800

    修改一些活动的参与规则

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Entities\Activity;
+use App\Entities\Participant;
 use App\Entities\Team;
 use App\Entities\WechatUser;
+use App\Jobs\MakeActivityQr;
 use App\User;
 use EasyWeChat;
-use Image;
 use Log;
-use QrCode;
 
 class ServerController extends Controller
 {
@@ -40,47 +40,103 @@ EOL;
 
                 case 'text':
                     $wechat_user = $this->getUser($open_id);
-                    $labels      = mb_substr($message->Content, 0, 1) . ',' . mb_substr($message->Content, -1, 1);
-                    Log::info('$labels', ['labels' => $labels]);
-                    $activity = Activity::whereLabels($labels)->first();
+                    $activity    = '';
+                    $team_name   = '';
+                    preg_match('/(.*)#(.*)#/', $message->Content, $matches);
+
+                    if (count($matches) > 2) {
+                        if (isset($matches[1]) && !empty($matches[1]) && isset($matches[2]) && !empty($matches[2])) {
+                            $team_name     = $matches[1];
+                            $activity_name = $matches[2];
+
+                            $activity = Activity::whereName($activity_name)->first();
+                        }
+                    } else {
+                        $activity = Activity::whereName($message->Content)->first();
+                    }
+
                     if ($activity) {
-                        $team_name = mb_substr($message->Content, 1, -1);
-                        $team      = Team::whereName($team_name)->whereActivityId($activity->id)->first();
-                        if ($team) {
-                            return '此活动中团队名 ' . $team_name . ' 已经存在，请换一个';
+                        // 创建团队
+                        if (isset($team_name)) {
+                            $team_name = mb_substr($message->Content, 1, -1);
+                            $team      = Team::whereName($team_name)->whereActivityId($activity->id)->first();
+                            if ($team) {
+                                return '"' . $activity->name . '"活动中团队名 ' . $team_name . ' 已经存在，请换一个';
+                            }
+
+                            $team = Team::whereUserId($wechat_user->user_id)->whereActivityId($activity->id)->first();
+                            if ($team) {
+                                return '您在"' . $activity->name . '"活动中已经创建过团队 ' . $team->name . ' 了';
+                            }
+                            $team = Team::create([
+                                'activity_id' => $activity->id,
+                                'name'        => $team_name,
+                                'user_id'     => $wechat_user->user_id,
+                                'count'       => 0,
+                            ]);
+
+                            $msg = <<<EOL
+您的团队已成功创建！分享下面的海报，获取好友支持。
+活动截止时间为 $activity->end_time ，我们将根据支持数选出排名前三的 3 个团队免费参加 $activity->name 的活动。
+如有问题，请咨询 15251909361。
+您可以回复关键词“ $activity->name ”或者扫描海报中的二维码随时查看团队支持情况。
+EOL;
+
+                            // 制作二维码，并以客服消息方式返回
+                            $this->dispatch(new MakeActivityQr($wechat_user, $activity, $team));
+
+                            return $msg;
+                        } else { // 查看支持或创建团队方式
+                            $team = Team::whereActivityId($activity->id)->whereUserId($wechat_user->user_id)->first();
+
+                            // 有创建
+                            if ($team) {
+                                $sort = Team::whereActivityId($activity->id)->where('count', '>', $team->count)->count() + 1;
+                                $msg  = <<<EOL
+感谢您参与“ $activity->name ”活动。
+
+您的团队支持情况如下：
+团队名称：$team->name
+支持人数：$team->count
+当前排名：$sort
+分享海报到朋友圈，让小伙伴们为你的团队助力！
+发送团队名称即可获取该团队海报。
+EOL;
+                            } else {
+                                $participant = Participant::whereActivityId($activity->id)->whereUserId($wechat_user->user_id)->first();
+                                // 有支持
+                                if ($participant) {
+                                    $sort = Team::whereActivityId($activity->id)->where('count', '>', $participant->team->count)->count() + 1;
+                                    $msg  = <<<EOL
+感谢您参与“ $activity->name ”活动。
+
+您目前支持的团队情况如下：
+团队名称：{$participant->team->name}
+支持人数：{$participant->team->count}
+当前排名：$sort
+您也可以创建自己的团队，赢取活动资格。回复团队名称+#{$activity->name}#，如：南京大学自2班#中秋免费游#，即可创建自己的团队。
+活动详情请点击 www.wecee.com 查看。
+EOL;
+                                } else { // 无支持
+                                    $msg = <<<EOL
+感谢您参与“{$activity->name}”活动，目前您还没有创建自己的团队，回复团队名称+#{$activity->name}#，如：南京大学自2班#中秋免费游#，即可创建自己的团队。
+活动详情请点击 www.wecee.com 查看。
+EOL;
+                                }
+                            }
+
+                            return $msg;
                         }
-
-                        $team = Team::whereUserId($wechat_user->user_id)->whereActivityId($activity->id)->first();
-                        if ($team) {
-                            return '您在此活动中已经创建过团队 ' . $team->name . ' 了';
-                        }
-                        $team    = Team::create([
-                            'activity_id' => $activity->id,
-                            'name'        => $team_name,
-                            'user_id'     => $wechat_user->user_id,
-                            'count'       => 0,
-                        ]);
-                        $url     = route('activity_team', ['activity_id' => $activity->id, 'team_id' => $team->id]);
-                        $img_url = $activity->full_pic_url;
-                        $image   = Image::make($img_url);
-
-                        /** @var \EasyWeChat\Material\Temporary $temp */
-                        $temp = EasyWeChat::material_temporary();
-                        $path = storage_path(uniqid() . '.png');
-                        $image->insert(QrCode::format('png')->size(100)->margin(0)->generate($url), 'center')->save($path);
-                        $result   = $temp->uploadImage($path);
-                        $media_id = $result->get('media_id');
-
-                        return new EasyWeChat\Message\Image(['media_id' => $media_id]);
                     } else {
                         $msg = <<<EOL
 你好，$wechat_user->nickname
 如果您需要客服帮助，请添加微信号：
 xuechun_1991
 EOL;
+
+                        return $msg;
                     }
 
-                    return $msg;
                     break;
             }
         });
---
+++ app/Jobs/MakeActivityQr.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Jobs;
+
+use EasyWeChat;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Image;
+use QrCode;
+
+class MakeActivityQr implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    private $wechat_user;
+    private $activity;
+    private $team;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($wechat_user, $activity, $team)
+    {
+        $this->wechat_user = $wechat_user;
+        $this->activity    = $activity;
+        $this->team        = $team;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $url     = route('activity_team', ['activity_id' => $this->activity->id, 'team_id' => $this->team->id]);
+        $img_url = $this->activity->full_pic_url;
+        $image   = Image::make($img_url);
+
+        /** @var \EasyWeChat\Material\Temporary $temp */
+        $temp = EasyWeChat::material_temporary();
+        $path = storage_path(uniqid() . '.png');
+        $image->insert(QrCode::format('png')->size(100)->margin(0)->generate($url), 'center')->save($path);
+        $result   = $temp->uploadImage($path);
+        $media_id = $result->get('media_id');
+
+        $message = new EasyWeChat\Message\Image(['media_id' => $media_id]);
+
+        /** @var \EasyWeChat\Staff\Staff $staff */
+        $staff  = EasyWeChat::staff();
+        $result = $staff->message($message)->to($this->wechat_user->open_id)->send();
+        \Log::info('make activity qr', ['user' => $this->wechat_user, 'activity' => $this
+            ->activity, 'team'                 => $this->team, 'result' => $result, ]);
+    }
+}
