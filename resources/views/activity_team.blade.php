6b076bd23c930b7479be12cc0166869a84408092
Connie <383108440@qq.com>
2017-08-21 16:47:34 +0800

    修改页面样式

--- resources/views/activity_team.blade.php
+++ resources/views/activity_team.blade.php
@@ -21,7 +21,7 @@
         <div class="box">
             <h2 class="team-name fz15 medium color-medium">团队名称：{{$team->name}}</h2>
             <p class="color-gray">当前共有 {{$team->participants()->count()}} 名支持者</p>
-            <p class="number artbrush">{{$sort}}</p>
+            <p >当前排名 <i class="number artbrush">{{$sort}}</i></p>
             @if(!$team->participants->isEmpty())
             <div class="avatars">
                 <p class="imgs clearfix">
