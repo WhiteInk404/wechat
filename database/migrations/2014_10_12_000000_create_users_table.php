7a648306553d3fbb6de318e3f13059d08b2b0ca3
jinchun <imjinchun@gmail.com>
2017-09-14 22:16:45 +0800

    兼容5.6 utf8mb4 index

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->tinyInteger('passport_type')->default(0)->comment('账户类型，0，普通，1 公众号，2 小程序');
-            $table->string('email')->unique();
+            $table->string('email',180)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->softDeletes();
